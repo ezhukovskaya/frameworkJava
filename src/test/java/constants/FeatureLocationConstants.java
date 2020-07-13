fd4f43390735a71b4b3056488198d9d34281009b
ekaterina.j <ekaterina.j@phpdev.org>
2020-07-13 17:16:44 +0300

    created TemplateTest.java

---
+++ src/test/java/constants/FeatureLocationConstants.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class FeatureLocationConstants {
+    public static final String TEMPLATE_PATH = "src/test/java/features/";
+    public static final String STEPS_DEFINITION_PACKAGE_NAME = "stepdefinitions";
+}
---
+++ src/test/java/testrunners/TemplateTest.java
@@ -0,0 +1,15 @@
+package testrunners;
+
+import constants.FeatureLocationConstants;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {FeatureLocationConstants.TEMPLATE_PATH},
+        glue = {FeatureLocationConstants.STEPS_DEFINITION_PACKAGE_NAME},
+        plugin = {"pretty"}
+)
+public class TemplateTest {
+}
