055591f8bb8c25393d1bd13e8dd8c7dbf9adfcac
ekaterina.j <ekaterina.j@phpdev.org>
2020-07-14 17:31:07 +0300

    Added Map case

--- src/test/java/features/Datatable.feature
+++ src/test/java/features/Datatable.feature
@@ -10,7 +10,16 @@ Feature: Data Tables
   @NewListOfStrings
   Scenario: Bill Amount Generation
     Given I placed an order for the following items
-      |Cucumber Sandwich|2|20|
-      |Cucumber Salad|1|15|
+    |Cucumber Sandwich|2    |20       |
+    |Cucumber Salad   |1    |15       |
+    When I generate the bill
+    Then a bill for $55 should be generated
+
+  @ListOfMaps
+  Scenario: Bill Amount Generation
+    Given I placed an order for the following items
+      |ItemName         |Units|UnitPrice|
+      |Cucumber Sandwich|2    |20       |
+      |Cucumber Salad   |1    |15       |
     When I generate the bill
     Then a bill for $55 should be generated
--- src/test/java/stepdefinitions/DataTablesSteps.java
+++ src/test/java/stepdefinitions/DataTablesSteps.java
@@ -7,22 +7,36 @@ import io.cucumber.java.en.When;
 import io.cucumber.java8.PendingException;
 
 import java.util.List;
+import java.util.Map;
 
 public class DataTablesSteps {
     @Given("^I placed an order for the following items$")
-    public void iPlacedAnOrderForTheFollowingItems(DataTable dataTable) throws Throwable {
-        List<List<String>> billData = dataTable.asLists(String.class);
-        billData.forEach(data->data.forEach(System.out::println));
+    public void iPlacedAnOrderForTheFollowingItems(DataTable dataTable) {
+        //1-DIMENSIONAL
+
+        /*List<String> billData = dataTable.asList(String.class);
+        billData.forEach(System.out::println);*/
+
+        //2-DIMENSIONAL
+
+       /* List<List<String>> billData = dataTable.asLists(String.class);
+        billData.forEach(data->data.forEach(System.out::println));*/
+
+        //MAP
+
+        List<Map<String, String>> billData = dataTable.asMaps(String.class, String.class);
+        billData.forEach(item -> item.forEach((key, value) -> System.out.println("Key: " + key)));
+        billData.forEach(item -> item.forEach((key, value) -> System.out.println("Value: " + value)));
     }
 
     @When("^I generate the bill$")
-    public void iGenerateTheBill() throws Throwable {
+    public void iGenerateTheBill() {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
     @Then("^a bill for \\$(\\d+) should be generated$")
-    public void aBillFor$ShouldBeGenerated(int arg1) throws Throwable {
+    public void aBillFor$ShouldBeGenerated(int arg1) {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
